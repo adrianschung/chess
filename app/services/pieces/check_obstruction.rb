6e7c4cfda448a782ae6db3cc5fd8c209cbe2e92f
adrianschung <adrianschung@gmail.com>
2018-09-23 17:00:58 -0400

    Adds diagonal check to check obstructions

--- app/services/pieces/check_obstruction.rb
+++ app/services/pieces/check_obstruction.rb
@@ -22,35 +22,58 @@ module Pieces
           obstructable_squares.push([piece.row, x])
         end
       else
-        for x in (piece.column + 1)...opponent_piece.column
+        for x in (piece.column + 1)...opponent_king.column
           obstructable_squares.push([piece.row, x])
         end
       end
-      obstructable_squares.each do |square|
-        new_square = { row: square[0], column: square[1] }
-        opponent_pieces.each do |piece|
-          return true if piece.valid_move?(new_square)
-        end
-      end
+      return true if valid_obstruction?(obstructable_squares)
+      false
     end
 
     def check_vertical_squares
       obstructable_squares = []
       if moving_up?
-        for x in (opponent_king.row + 1)...piece.row
-          obstructable_squares.push([x, piece.column])
+        for y in (opponent_king.row + 1)...piece.row
+          obstructable_squares.push([y, piece.column])
         end
       else
-        for x in (piece.row + 1)...opponent_piece.row
-          obstructable_squares.push([x, piece.column])
+        for x in (piece.row + 1)...opponent_king.row
+          obstructable_squares.push([y, piece.column])
         end
       end
-      obstructable_squares.each do |square|
-        new_square = { row: square[0], column: square[1] }
-        opponent_pieces.each do |piece|
-          return true if piece.valid_move?(new_square)
+      return true if valid_obstruction?(obstructable_squares)
+      false
+    end
+
+    def check_diagonal_squares
+      obstructable_squares = []
+      if moving_up? && moving_left?
+        for x in (opponent_king.column + 1)...piece.column
+          for y in (opponent_king.row + 1)...piece.row
+            obstructable_squares.push([y, x])
+          end
+        end
+      elsif moving_up?
+        for x in (piece.column + 1)...opponent_piece.column
+          for y in for y in (opponent_king.row + 1)...piece.row
+            obstructable_squares.push([y, x])
+          end
+        end
+      elsif moving_left?
+        for x in (opponent_king.column + 1)...piece.column
+          for y in (piece.row + 1)...opponent_king.row
+            obstructable_squares.push([y, x])
+          end
+        end
+      else
+        for x in (piece.column + 1)...opponent_piece.column
+          for y in (piece.row + 1)...opponent_king.row
+            obstructable_squares.push([y, x])
+          end
         end
       end
+      return true if valid_obstruction?(obstructable_squares)
+      false
     end
 
     def moving_up?
@@ -67,6 +90,16 @@ module Pieces
 
     def opponent_pieces
       opponent_pieces = piece.opponent_pieces
+    end
+
+    def valid_obstruction?(squares)
+      squares.each do |square|
+        new_square = { row: square[0], column: square[1] }
+        opponent_pieces.each do |piece|
+          return true if piece.valid_move?(new_square)
+        end
+      end
+      false
     end
 
     attr_accessor :game, :piece, :new_square
