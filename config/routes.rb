681b525b69f0c4e1751d93b999dc828cb275950d
adrianschung <adrianschung@gmail.com>
2018-09-25 23:15:34 -0400

    Adds user profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
     put '/castle' => 'special_moves#castle'
   end
   resources :pieces, only: :update
+  resources :players, only: :show
+  get 'myprofile' => 'users#myprofile'
 end
