5b9b5fa2a11a20e289727611162c684ba7817f8c
adrianschung <adrianschung@gmail.com>
2018-10-29 21:01:17 -0400

    Add conversation and message tables to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024150741) do
+ActiveRecord::Schema.define(version: 20181030010006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer  "white_player_id"
@@ -23,6 +30,17 @@ ActiveRecord::Schema.define(version: 20181024150741) do
     t.datetime "updated_at",                  null: false
     t.string   "name"
     t.index ["white_player_id", "black_player_id"], name: "index_games_on_white_player_id_and_black_player_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "pieces", force: :cascade do |t|
