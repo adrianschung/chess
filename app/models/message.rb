60139975a81a51067e97be60de538ad2d3422780
adrianschung <adrianschung@gmail.com>
2018-11-01 17:42:04 -0400

    Add message/conversation models

---
+++ app/models/message.rb
@@ -0,0 +1,10 @@
+class Message < ApplicationRecord
+  belongs_to :conversation
+  belongs_to :player
+
+  validates_presence_of :body, :conversation_id, :player_id
+
+  def message_time
+    created_at.strftime("%m/%d/%y at %l:%M %p")
+  end
+end
