a0dfd01fee1f05cac5d919cd7641fe42d9fc357f
adrianschung <adrianschung@gmail.com>
2018-11-26 09:50:23 -0500

    Fix game redirect bug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -65,8 +65,6 @@ class GamesController < ApplicationController
     if current_player != current_game.white_player && current_player != current_game.black_player
       flash[:error] = 'You are not participating in this game'
       redirect_to games_path
-    else
-      render game_path(current_game)
     end
   end
 end
