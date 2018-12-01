2de42cd71d3188536d411e35de546c2179cfcc52
adrianschung <adrianschung@gmail.com>
2018-11-30 23:26:10 -0500

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filted :set_layout_variables
+  before_filter :set_layout_variables
   
   def after_sign_in_path_for(*)
     games_path
