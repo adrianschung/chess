685c2889c7682654ce4c4cc9651c1a3b74fd9bcc
adrianschung <adrianschung@gmail.com>
2018-12-03 17:38:11 -0500

    Update home page

--- app/services/pieces/move_to.rb
+++ app/services/pieces/move_to.rb
@@ -10,7 +10,6 @@ module Pieces
       return Pieces::Capture.call(piece, new_square) if game.square_occupied?(new_square)
       piece.update_attributes(column: new_square[:column], row: new_square[:row])
       piece.increment!(:moves)
-      true
     end
 
     private
