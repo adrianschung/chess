577ba07d35179a9b71f86f0cfd4703931b86daba
adrianschung <adrianschung@gmail.com>
2018-12-19 16:43:30 -0500

    Finish moving castling from button to normal move

--- app/models/king.rb
+++ app/models/king.rb
@@ -6,11 +6,29 @@ class King < Piece
     self.picture = 'blackking.png' if color == 'Black'
   end
 
+  def move_to!(new_square)
+    transaction do
+      if valid_move?(new_square)
+        Pieces::MoveTo.call(self, new_square)
+        Games::UpdateState.call(game)
+      end
+      if castle?(new_square)
+        if new_square[:column] == 2
+          @rook.update_attributes(column: 3)
+          self.update_attributes(column: 2)
+        elsif new_square[:column] == 6
+          @rook.update_attributes(column: 5)
+          self.update_attributes(column: 6)
+        end
+      raise ActiveRecord::Rollback if game.check?(player)
+    end
+  end
+
   def valid_move?(new_square)
     super &&
       valid_movement?(1, -1, new_square[:row] - row) &&
       valid_movement?(1, -1, new_square[:column] - column) &&
-      valid_space?(new_square) || castle(new_square)
+      valid_space?(new_square)
   end
 
   def castle!(direction)
@@ -35,13 +53,14 @@ class King < Piece
     end
   end
 
-  def castle(new_space)
-    return false if !rook || self.moves != 0 || Pieces::Obstruction.call(self, rook)
-    return false unless new_space[:column] == 2 || new_space[:column] == 7
+  def castle?(new_space)
+    @rook = nil
+    return false unless new_space[:column] == 2 || new_space[:column] == 7 || self.moves == 0
     if new_space[:column] == 2
-      rook = game.pieces.where(row: new_space[:row], column: 0, type: 'Rook', captured: false).first
+      @rook = game.pieces.where(row: new_space[:row], column: 0, type: 'Rook', captured: false).first
     elsif new_space[:column] == 6
-      rook = game.pieces.where(row: new_space[:row], column: 7, type: 'Rook', captured: false).first
+      @rook = game.pieces.where(row: new_space[:row], column: 7, type: 'Rook', captured: false).first
     end
+    return false if !@rook || Pieces::Obstruction.call(self, @rook)
   end
 end
