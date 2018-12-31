56b6293c0451cc9748293732db66dbaa9f719cb3
adrianschung <adrianschung@gmail.com>
2018-12-31 14:27:44 -0500

    remove castle from special moves controller

--- app/controllers/special_moves_controller.rb
+++ app/controllers/special_moves_controller.rb
@@ -1,10 +1,4 @@
 class SpecialMovesController < ApplicationController
-  def castle
-    king = current_game.pieces.where(type: 'King').find_by(player: get_player(current_game))
-    Pieces::King::Castle.call(king, castle_params[:direction])
-    redirect_to game_path(current_game)
-  end
-
   def edit_promotion
     current_game
   end
@@ -15,10 +9,6 @@ class SpecialMovesController < ApplicationController
   end
 
   private
-
-  def castle_params
-    params.permit(:direction, :game_id)
-  end
 
   def piece_params
     params.require(:piece).permit(:type)
@@ -34,10 +24,5 @@ class SpecialMovesController < ApplicationController
 
   def pawn
     game_pieces.where(row: 7).or(game_pieces.where(row: 0).where(type: 'Pawn'))
-  end
-
-  def get_player(game)
-    return game.white_player if game.state == "White's Turn"
-    return game.black_player if game.state == "Black's Turn"
   end
 end
