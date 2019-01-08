e026342cd83b76760a7e9bbb25563e904d8093b6
adrianschung <adrianschung@gmail.com>
2019-01-07 22:05:52 -0500

    Fix edit profile page and avatar uploading

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,20 +1,22 @@
 class PlayersController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_player!, only: [:edit, :update]
   
   def show
   end
 
   def edit
+    @player = current_player
   end
 
   def update
     current_player.update_attributes(player_params)
+    redirect_to edit_player_path(current_player)
   end
 
   private
 
   def player_params
-    params.permit(:avatar, :country, :birthyear)
+    params.require(:player).permit(:avatar)
   end
 
   helper_method :player_profile
