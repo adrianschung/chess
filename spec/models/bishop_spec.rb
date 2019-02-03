6c3ac616ee2d652c822df5b4f19c69785b002fd4
adrianschung <adrianschung@gmail.com>
2019-02-03 16:03:47 -0500

    Add extra bishop tests

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe Bishop, type: :model do
     game = FactoryBot.create(:game, white_player: player)
     bishop = FactoryBot.create(:bishop, game: game, player: player)
 
-    it 'does not allow the bishop to move in only 1 direction' do
-      # move column only
+    it 'does not allow the bishop to move in only horizontal direction' do
       bishop.move_to!(row: 1, column: 5)
       expect(bishop.row).to eq(1)
       expect(bishop.column).to eq(7)
-      # move rows only
+    end
+
+    it 'does not allow the bishop to move in only vertical direction' do
       bishop.move_to!(row: 3, column: 7)
       expect(bishop.row).to eq(1)
       expect(bishop.column).to eq(7)
     end
-    it 'allows the bishop to move the same amount of rows and same amount of columns' do
+
+    it 'allows the bishop to move in the correct diagonal direction' do
       bishop.move_to!(row: 2, column: 6)
       expect(bishop.row).to eq(2)
       expect(bishop.column).to eq(6)
