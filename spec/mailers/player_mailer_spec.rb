213d0f88bd8052acf3f52da5e2dd44a724c71ddc
adrianschung <adrianschung@gmail.com>
2019-04-22 14:42:24 -0400

    Add test for message sending email

--- spec/mailers/player_mailer_spec.rb
+++ spec/mailers/player_mailer_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe PlayerMailer, type: :mailer do
       expect(mail.body.encoded).to match(game_url(game))
     end
   end
+
+  describe 'message sent' do
+    let(:sender) { FactoryBot.create(:player, playername: 'Sender') }
+    let(:recipient) { FactoryBot.create(:player, playername: 'Recipient') }
+    let(:conversation) { Conversation.create(sender_id: sender.id, recipient_id: recipient.id) }
+    let(:message) { Message.create(body:'test', conversation_id: conversation.id, player_id: sender.id) }
+    let(:mail) { PlayerMailer.message_sent(message)}
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq('A player has sent you a message')
+      expect(mail.to).to eq([recipient.email])
+      expect(mail.from).to eq(["do-not-reply@chesssenders.com"])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match(recipient.playername)
+      expect(mail.body.encoded).to match(sender.playername)
+      expect(mail.body.encoded).to match(conversation_messages_url(conversation))
+    end
+  end
 end
