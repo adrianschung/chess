15d4f9ecf58e1837e115762b32cad3530165b0fd
adrianschung <adrianschung@gmail.com>
2019-04-25 08:59:42 -0400

    Use actioncable to update views without refresh

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,14 +4,14 @@ class PiecesController < ApplicationController
     current_piece.move_to!(new_square_params)
     flash[:notice] = "#{piece_player.playername} is in check" if game.check?(piece_player)
     game.check_endgame(current_piece.player)
-    GameChannel.broadcast_to(game, game_json)
+    @board = Games::RenderChessboard.call(game.pieces.where(captured: false))
+    GameChannel.broadcast_to(game, view: ApplicationController.render(
+                                    partial: 'games/chessboard',
+                                    locals: { chess_board: @board } )
+                            )
   end
 
   private
-
-  def chess_board
-    Games::RenderChessboard.call(game.pieces.where(captured: false))
-  end
 
   def new_square_params
     @params = params.permit(:row, :column, :id)
@@ -24,12 +24,6 @@ class PiecesController < ApplicationController
 
   def game
     @game = current_piece.game
-  end
-
-  def game_json
-    game.as_json(include: { pieces: { 
-                              only: [:id, :row, :column, :player_id, :picture]  }},
-                    only: :state)
   end
 
   def piece_player
