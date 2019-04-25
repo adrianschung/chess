9207836a46b53c205020e888ff59290454e7ec56
adrianschung <adrianschung@gmail.com>
2019-04-25 17:27:45 -0400

    Prevent users from moving opponents pieces

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,17 +1,20 @@
 class PiecesController < ApplicationController
 
   def update
+    return redirect_to game_path(game) if current_player.id != current_piece.player_id
     current_piece.move_to!(new_square_params)
-    flash[:notice] = "#{piece_player.playername} is in check" if game.check?(piece_player)
     game.check_endgame(current_piece.player)
-    @board = Games::RenderChessboard.call(game.pieces.where(captured: false))
     GameChannel.broadcast_to(game, view: ApplicationController.render(
                                     partial: 'games/chessboard',
-                                    locals: { chess_board: @board } )
+                                    locals: { chess_board: board } )
                             )
   end
 
   private
+
+  def board
+    Games::RenderChessboard.call(game.pieces.where(captured: false))
+  end
 
   def new_square_params
     @params = params.permit(:row, :column, :id)
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,6 @@
 module GamesHelper
   def set_class(piece)
     return 'droppable' if piece.nil?
-#    return if current_player.id != piece.player_id
     Pieces::Disabled.call(piece) ? 'draggable' : 'droppable'
   end
 end
