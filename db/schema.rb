14ab47523c5ae7776044d3fb5e691e11cc41e15b
adrianschung <adrianschung@gmail.com>
2019-05-01 17:05:17 -0400

    Add timestamps to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123233404) do
+ActiveRecord::Schema.define(version: 20190501210214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,12 @@ ActiveRecord::Schema.define(version: 20190123233404) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text    "body"
-    t.integer "conversation_id"
-    t.integer "player_id"
-    t.boolean "read",            default: false
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "player_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
     t.index ["player_id"], name: "index_messages_on_player_id", using: :btree
   end
