dac228d4d5229d93d6bebdc068b6f74b3b0aab5d
adrianschung <adrianschung@gmail.com>
2019-05-14 22:24:14 -0400

    Fix mailer spec rubocop errors

--- spec/mailers/player_mailer_spec.rb
+++ spec/mailers/player_mailer_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe PlayerMailer, type: :mailer do
     it 'renders the headers' do
       expect(mail.subject).to eq('A player has joined your game')
       expect(mail.to).to eq([white.email])
-      expect(mail.from).to eq(["do-not-reply@chesssenders.com"])
+      expect(mail.from).to eq(['do-not-reply@chesssenders.com'])
     end
 
     it 'renders the body' do
@@ -24,13 +24,17 @@ RSpec.describe PlayerMailer, type: :mailer do
     let(:sender) { FactoryBot.create(:player, playername: 'Sender') }
     let(:recipient) { FactoryBot.create(:player, playername: 'Recipient') }
     let(:conversation) { Conversation.create(sender_id: sender.id, recipient_id: recipient.id) }
-    let(:message) { Message.create(body:'test', conversation_id: conversation.id, player_id: sender.id) }
-    let(:mail) { PlayerMailer.message_sent(message)}
+    let(:message) {
+      Message.create(body: 'test',
+                     conversation_id: conversation.id,
+                     player_id: sender.id)
+    }
+    let(:mail) { PlayerMailer.message_sent(message) }
 
     it 'renders the headers' do
       expect(mail.subject).to eq('A player has sent you a message')
       expect(mail.to).to eq([recipient.email])
-      expect(mail.from).to eq(["do-not-reply@chesssenders.com"])
+      expect(mail.from).to eq(['do-not-reply@chesssenders.com'])
     end
 
     it 'renders the body' do
